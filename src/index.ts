f579bc4a8d4d6243239564a573ffbf3c8377f18e
OverlineJunior <franciscosebastianyjunior@hotmail.com>
2022-09-03 01:43:38 -0300

    start.

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+interface Event {
+	[state: string]: string;
+}
+
+interface FlowMap {
+	[eventName: string]: Event;
+}
+
+interface LockLayers {
+	[eventName: string]: number;
+}
+
+function freshLockLayers(flowMap: FlowMap): LockLayers {
+	const lockLayers: LockLayers = {};
+
+	for (const [eventName] of pairs(flowMap)) {
+		lockLayers[eventName] = 0;
+	}
+
+	return lockLayers;
+}
+
+export class StateMachine<Transitions extends FlowMap> {
+	readonly state: string;
+	readonly trigger?: keyof Transitions;
+	private readonly flowMap: Transitions;
+	private readonly lockLayers: LockLayers;
+
+	constructor(flowMap: Transitions, initialState: string, _trigger?: keyof Transitions, _lockLayers?: LockLayers) {
+		this.flowMap = flowMap;
+		this.state = initialState;
+		this.trigger = _trigger;
+		this.lockLayers = _lockLayers !== undefined ? _lockLayers : freshLockLayers(flowMap);
+	}
+
+	public transition(eventName: keyof Transitions) {
+		const event = this.flowMap[eventName];
+		const newState = event[this.state];
+
+		return newState === this.state ? this : new StateMachine(this.flowMap, newState, eventName, this.lockLayers);
+	}
+
+	public lock(eventName: string) {
+		const newLockLayers = table.clone(this.lockLayers);
+		newLockLayers[eventName] += 1;
+
+		return new StateMachine(this.flowMap, this.state, this.trigger, newLockLayers);
+	}
+
+	public unlock(eventName: string) {
+		if (this.lockLayers[eventName] === 0) return;
+
+		const newLockLayers = table.clone(this.lockLayers);
+		newLockLayers[eventName] -= 1;
+
+		return new StateMachine(this.flowMap, this.state, this.trigger, newLockLayers);
+	}
+
+	public isLocked(eventName: string) {
+		return this.lockLayers[eventName] !== 0;
+	}
+
+	public can(eventName: string) {
+		const event = this.flowMap[eventName];
+
+		return event[this.state] !== undefined && !this.isLocked(eventName);
+	}
+}
+
+let lock = new StateMachine(
+	{
+		TurnKey: {
+			Locked: "Unlocked",
+			Unlocked: "Locked",
+		},
+
+		Break: {
+			Locked: "Broken",
+			Unlocked: "Broken",
+		},
+	},
+	"Locked",
+);
+
+lock = lock.transition("TurnKey");
